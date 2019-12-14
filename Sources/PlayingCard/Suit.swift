433c50a23490e56321dd10b92411975d3deb3144
Go7hic <gtfx0209@qq.com>
2019-12-14 14:02:24 +0800

    first commit

---
+++ Sources/PlayingCard/Suit.swift
@@ -0,0 +1,32 @@
+
+public enum Suit: String, CaseIterable {
+    case spades, hearts, diamonds, clubs
+}
+
+// MARK: - Comparable
+extension Suit: Comparable {
+    public static func <(lhs: Suit, rhs: Suit) -> Bool {
+        switch (lhs, rhs) {
+        case (_, _) where lhs == rhs:
+            return false
+        case (.spades, _),
+        (.hearts, .diamonds), (.hearts, .clubs),
+        (.diamonds, .clubs):
+            return false
+        default:
+            return true
+        }
+    }
+}
+
+// MARK: - CustomStringConvertible
+extension Suit: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .spades: return "♠︎"
+        case .hearts: return "♡"
+        case .diamonds: return "♢"
+        case .clubs: return "♣︎"
+        }
+    }
+}
